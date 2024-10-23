d3ed8092bae9d69685f441ab96b9252ee4c5ac50
Jorge Sardon <anibal317@gmail.com>
2024-10-23 16:20:24 -0300

    exporting Facilities module

--- src/facilities/facilities.module.ts
+++ src/facilities/facilities.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FacilitiesService } from './facilities.service';
 import { FacilitiesController } from './facilities.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([FacilitiesModule])],
   controllers: [FacilitiesController],
   providers: [FacilitiesService],
 })
