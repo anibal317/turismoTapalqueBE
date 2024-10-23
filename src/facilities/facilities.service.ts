7d81f5d496f4b0623d40f5c49487336505c62a89
Jorge Sardon <anibal317@gmail.com>
2024-10-23 16:41:00 -0300

    adding endpoints

--- src/facilities/facilities.service.ts
+++ src/facilities/facilities.service.ts
@@ -1,26 +1,84 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateFacilityDto } from './dto/create-facility.dto';
 import { UpdateFacilityDto } from './dto/update-facility.dto';
+import { Facility } from './entities/facility.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class FacilitiesService {
-  create(createFacilityDto: CreateFacilityDto) {
-    return 'This action adds a new facility';
+
+  constructor(
+    @InjectRepository(Facility)
+    private readonly facilityRepository: Repository<Facility>,
+  ) { }
+
+  async create(createFacilityDto: CreateFacilityDto) {
+    try {
+      return await this.facilityRepository.save(createFacilityDto);
+    } catch (error) {
+      throw new HttpException(`Error creating Type Entity: ${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-  findAll() {
-    return `This action returns all facilities`;
+  async findAll() {
+    try {
+      const types = await this.facilityRepository.find({ relations: ['users', 'subtype', 'cityPoints'] });
+      if (types.length === 0) {
+        throw new HttpException('No Type Entities found', HttpStatus.NO_CONTENT);
+      }
+      return types;
+    } catch (error) {
+      throw new HttpException(`Error fetching Type Entities: ${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} facility`;
+  async findOne(id: number) {
+    if (!id || isNaN(id)) {
+      throw new HttpException('Invalid ID', HttpStatus.BAD_REQUEST);
+    }
+
+    const typeEntity = await this.facilityRepository.findOne({ where: { id } });
+    if (!typeEntity) {
+      throw new HttpException('Type Entity not found', HttpStatus.NOT_FOUND);
+    }
+
+    return typeEntity;
   }
 
-  update(id: number, updateFacilityDto: UpdateFacilityDto) {
-    return `This action updates a #${id} facility`;
+  async update(id: number, updateFacilityDto: UpdateFacilityDto) {
+    if (!id || isNaN(id)) {
+      throw new HttpException('Invalid ID', HttpStatus.BAD_REQUEST);
+    }
+
+    const typeEntity = await this.facilityRepository.findOne({ where: { id } });
+    if (!typeEntity) {
+      throw new HttpException('Type Entity not found', HttpStatus.NOT_FOUND);
+    }
+
+    try {
+      await this.facilityRepository.update(id, updateFacilityDto);
+      return this.findOne(id); // Devuelve la entidad actualizada
+    } catch (error) {
+      throw new HttpException(`Error updating Type Entity: ${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} facility`;
+  async remove(id: number) {
+    if (!id || isNaN(id)) {
+      throw new HttpException('Invalid ID', HttpStatus.BAD_REQUEST);
+    }
+
+    const typeEntity = await this.facilityRepository.findOne({ where: { id } });
+    if (!typeEntity) {
+      throw new HttpException('Type Entity not found', HttpStatus.NOT_FOUND);
+    }
+
+    try {
+      await this.facilityRepository.delete(id);
+      return { message: 'Type Entity deleted successfully' };
+    } catch (error) {
+      throw new HttpException(`Error deleting Type Entity: ${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
