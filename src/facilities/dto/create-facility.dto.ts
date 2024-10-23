8c75fd9fad6a5524607c0e0175eff942aa33d55d
Jorge Sardon <anibal317@gmail.com>
2024-10-23 16:29:43 -0300

    updating facility dto

--- src/facilities/dto/create-facility.dto.ts
+++ src/facilities/dto/create-facility.dto.ts
@@ -1,1 +1,11 @@
-export class CreateFacilityDto {}
+import { IsOptional, IsString } from "class-validator"
+
+export class CreateFacilityDto {
+    @IsString()
+    name: string
+
+    @IsString()
+    @IsOptional()
+    description?: string
+
+}
